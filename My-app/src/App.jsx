6e755b59a37eeb556b0b123b48b6456c5955cb91
Taimoor <taimooransari072@gmail.com>
2026-04-24 22:05:14 +0500

    first commit

---
+++ My-app/src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import Header from "./component/Header";
+import Main from "./component/Main";
+import FuleSection from "./component/FuleSection";
+import Testimonials from "./component/Testimonials";
+import Footer from "./component/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <FuleSection />
+      <Testimonials />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
